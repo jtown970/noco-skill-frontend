452fff783c3c2e504e52f833c592d0d5c1c691f9
jtown970 <jasonrtowner@gmail.com>
2023-05-11 17:07:01 -0600

    changed url to be with db hosted site

--- src/utils/newRequest.js
+++ src/utils/newRequest.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
+const url = 'https://noco-skills-backend.adaptable.app/api/'
 const newRequest = axios.create({
-  baseURL: "http://localhost:8000/api/",
+  baseURL: url,
   withCredentials: true,
 });
 
