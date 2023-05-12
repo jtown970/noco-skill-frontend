4db1502543f74aa4373f8150b5157fde63e0d91f
jtown970 <jasonrtowner@gmail.com>
2023-05-12 09:48:07 -0600

    changed url for api's to working onrender backend url

--- src/utils/newRequest.js
+++ src/utils/newRequest.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = 'https://noco-skills-backend.adaptable.app/api/'
+const url = 'https://noco-skills-backend.onrender.com/api/'
 const newRequest = axios.create({
   baseURL: url,
   withCredentials: true,
