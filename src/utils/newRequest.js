5312c286dd58348912d50f804362623ffc48e4af
jtown970 <jasonrtowner@gmail.com>
2023-05-15 17:09:17 -0600

    changed back to onrender

--- src/utils/newRequest.js
+++ src/utils/newRequest.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = 'https://api.nocoskills.com/api/'
+const url = 'https://noco-skills-backend.onrender.com/api/'
 const newRequest = axios.create({
   baseURL: url,
   withCredentials: true,
